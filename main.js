75450a8f0e43786b3ce1ee09feeb6db5a27b2f6d
Rebecca Choo <rnwldms@gmail.com>
2021-01-14 23:59:42 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -9,13 +9,35 @@ function create(item){
     const sourceCode = `<li class='item' data-type='${item.type}' data-color='${item.color}'><i class="btn ${item.className} ${item.color}"></i><span>${item.type}, ${item.size}</span><li>`; 
     return sourceCode;
 }
-// 
-function onButtonClick(event, items) {
-    console.log(items);
+// When a button is clicked, call updateItems function to change the screen
+function onButtonClick(event) {
+    const target = event.target;
+    const key = target.dataset.key;
+    const value = target.dataset.value;
+    if (key == null || value == null) {
+        return;
+    } 
+    // items are the list of all items in JSON file
+    updateItems(key, value);
 }
-//
-function updateItems(items, key, value) {
-
+function showAll(event) {
+    const buttons = document.querySelectorAll('.item'); 
+    buttons.forEach(button => { 
+        button.classList.remove('invisible'); 
+    });
+}
+// Update items according to the selection
+function updateItems(key, value) {  
+    // get all JSON file items to sort
+    const buttons = document.querySelectorAll('.item'); 
+    // when key and value are matched with the selection, it will remove the invisible class
+    buttons.forEach(button => {
+        if(button.dataset[key] === value) {
+            button.classList.remove('invisible');
+        }else {
+            button.classList.add('invisible');
+        }
+    });
 }
 // Execute this code
 fetchAll()
@@ -23,9 +45,10 @@ fetchAll()
     const elements = items.map(create); 
     const container = document.querySelector('.items');
     container.innerHTML = elements.join('');
-    const buttons = document.querySelectorAll('i.fruit-btn'); 
-    buttons.forEach(button => {
-        console.log(button);
-        button.addEventListener('click', event => onButtonClick(event, elements));
+    const buttons = document.querySelectorAll('i.fruit-btn, button.fruit-btn'); 
+    buttons.forEach(button => {  
+        button.addEventListener('click', event => onButtonClick(event));
     });
+    const logo = document.querySelector('.reset__btn');
+    logo.addEventListener('click', event => showAll(event));
 });
